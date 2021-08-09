d059c8e62fedeb65646235e2471158d6cfaa8a9b
Sudarsna Mukund <56210484+sumukund@users.noreply.github.com>
2021-08-09 15:53:27 -0400

    beaches
    heaux and trixs

---
+++ midterm/root/assets/js/hideaway.js
@@ -0,0 +1,30 @@
+// JavaScript Document
+
+
+function myFunction() {
+document.getElementById('hideaway').style.display='block';
+document.getElementsById("quote")[0].setAttribute("class", "paragraph"); 
+
+}
+
+function myFunction2(){
+	
+	document.getElementById('hideaway2').style.display='block';
+	document.getElementsById("quote2")[0].setAttribute("class", "paragraph2"); 
+
+}
+
+function myFunction3(){
+	
+	document.getElementById('hideaway3').style.display='block';
+	document.getElementsById("quote3")[0].setAttribute("class", "paragraph3"); 
+
+}
+
+function myFunction4(){
+	
+	document.getElementById('hideaway4').style.display='block';
+	document.getElementsById("quote4")[0].setAttribute("class", "paragraph4"); 
+
+}
+
